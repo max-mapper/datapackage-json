d28b5493c702ff0e47ebead4e825c0494692b524
Max Ogden <max@maxogden.com>
2013-11-08 13:34:04 +0000

    add default, write, prompt methods

--- bin.js
+++ bin.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
-require('./')()
+
+var cmd = process.argv[2]
+if (!cmd) cmd = 'default'
+if (['default', 'prompt'].indexOf(cmd) === -1) cmd = 'default'
+
+var dpj = require('./')()
+dpj[cmd](function(err, def) {
+  if (def) console.log(def)
+})
--- index.js
+++ index.js
@@ -1,12 +1,98 @@
-var init = require('init-package-json')
+var fs = require('fs')
+var vm = require('vm')
+var util = require('util')
 var path = require('path')
+var Module = require('module').Module
+var init = require('init-package-json')
 
-module.exports = function() {
-  var initFile = path.join(__dirname, 'prompt.js')
-  var dir = process.cwd()
+module.exports = DPJ
 
-  init(dir, initFile, function (er, data) {
+function DPJ(file, dir) {
+  if (!(this instanceof DPJ)) return new DPJ(file, dir)
+  this.file = file || path.join(__dirname, 'prompt.js')
+  this.dir = dir || process.cwd()
+}
+
+DPJ.prototype.prompt = function() {
+  init(this.dir, this.file, function (er, data) {
     if (er) console.error('\n' + er.message)
   })  
 }
 
+DPJ.prototype.write = function(obj, saveTarget, cb) {
+  var defaultPath = path.join(this.dir, 'package.json')
+  
+  if (typeof saveTarget === 'function') {
+    cb = saveTarget
+    saveTarget = defaultPath
+  }
+  
+  if (typeof saveTarget === 'undefined')
+    saveTarget = defaultPath
+  
+  obj = JSON.stringify(obj, null, 2) + "\n"
+  fs.writeFile(saveTarget, data, function (er) {
+    if (cb) cb(er)
+  })  
+}
+
+// taken from PromZard
+// runs prompt script in a vm to generate a package.json w/ defaults
+DPJ.prototype.default = function(cb) {
+  var self = this
+  
+  var context = {
+    prompt: function(prop, defaultValue) { return defaultValue },
+    filename: this.file,
+    dirname: path.dirname(this.file),
+    basename: path.basename(this.file),
+    config: {}
+  }
+  
+  context.module = this.makeModule()
+  context.require = function (path) { return context.module.require(path) }
+  context.resolve = function(path) { return Module._resolveFilename(path, context.module) }
+  context.exports = context.module.exports
+  
+  fs.readFile(self.file, function(err, buff) {
+    var script = self.wrap(buff.toString(), context)
+    executePrompt(script)
+  })
+  
+  function executePrompt(script) {
+    var fn = vm.runInThisContext(script, self.file)
+    var args = Object.keys(context).map(function (k) {
+      return context[k]
+    }.bind(self))
+    try { var res = fn.apply(context, args) }
+    catch (er) { cb(er) }
+    var result
+    if (res &&
+        typeof res === 'object' &&
+        exports === context.module.exports &&
+        Object.keys(exports).length === 1) {
+      result = res
+    } else {
+      result = context.module.exports
+    }
+    cb(false, result)
+    
+  }
+}
+
+// taken from PromZard
+DPJ.prototype.wrap = function (body, context) {
+  var s = '(function( %s ) { %s\n })'
+  var args = Object.keys(context).join(', ')
+  return util.format(s, args, body)
+}
+
+// taken from PromZard
+DPJ.prototype.makeModule = function() {
+  var mod = new Module(this.file, module)
+  mod.loaded = true
+  mod.filename = this.file
+  mod.id = this.file
+  mod.paths = Module._nodeModulePaths(path.dirname(this.file))
+  return mod
+}
